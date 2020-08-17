bcdd7ef767e0b5b05de4136a5267aae509c42f91
maxpiano <maxbilov@gmail.com>
2020-08-17 22:03:17 +0200

    step2

---
+++ components/Joke.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="joke">
+    <p>{{ joke }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['joke', 'id'],
+  };
+</script>
+
+<style>
+  .joke {
+    padding: 1rem;
+    border: 1px dotted #ccc;
+    margin: 1rem 0;
+  }
+</style>
--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -1,9 +1,45 @@
 <template>
-  <div>jokes</div>
+  <div>
+    <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke" />
+  </div>
 </template>
 
 <script>
-  export default {};
+  import axios from 'axios';
+
+  export default {
+    name: 'JokesPage',
+    data() {
+      return {
+        jokes: [],
+      };
+    },
+    async created() {
+      const config = {
+        headers: {
+          Accept: 'application/json',
+        },
+      };
+      try {
+        const res = await axios.get('https://icanhazdadjoke.com/search', config);
+        this.jokes = res.data.results;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    head() {
+      return {
+        title: 'Jokes',
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'the best of the best',
+          },
+        ],
+      };
+    },
+  };
 </script>
 
 <style>
