d13fc5b4f525c74bebb2d568f45e7ed620e81e06
maxpiano <maxbilov@gmail.com>
2020-08-17 18:01:52 +0200

    makin some pages

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>About DadJokes</h1>
+    <p>This is an app</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    head() {
+      return {
+        title: 'About Page',
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'the best of the best',
+          },
+        ],
+      };
+    },
+  };
+</script>
+
+<style>
+</style>
